2034e2e35143ac424340ec0cd1d9c97495836eda
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2024-05-08 22:56:14 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -271,7 +271,7 @@ def create_mod():
     ratio_value = (int(numerator_entry.get()) / int(denominator_entry.get()))
     scaling_factor = (16/9) / (int(numerator_entry.get()) / int(denominator_entry.get()))
     username = getpass.getuser()
-    gameid = "0100A3900C3E2000"
+    gameid = "01007EF00011E000"
     if output_yuzu.get() is True:
         input_folder = f"C:/Users/{username}/AppData/Roaming/yuzu/load/{gameid}"
         process_name = "yuzu.exe"
