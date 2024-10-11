38c648f78edcaa5b7de5df0ffc8aa2c3bbfbb174
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2024-10-11 00:47:11 -0400

    Update script.py

--- script.py
+++ script.py
@@ -11,8 +11,13 @@ def patch_blarc(aspect_ratio, HUD_pos, romfs_folder):
     file_paths = {}
 
     layout_map = {
-                    'Title_00': ['N_CaptureLogo_00', "N_Capture_01", "N_CaptureTrial_00", "N_CaptureTrialSh_00"],
+                    'Title_00': ['N_CaptureLogo_00', "N_Capture_01", "N_CaptureTrial_00", "N_CaptureTrialSh_00", 
+                                'N_Trial_00', "Pa_SaveFile_00", "Pa_SaveFile_01", "Pa_SaveFileHardMode_00", 'Pa_SaveFile_02', 
+                                "Pa_SaveFile_03", "Pa_SaveFile_04", "Pa_SaveFile_05", "P_TrialSh_00", "P_Trial_01",
+                                "T_Nintendo_00", "T_Nintendo_00_TW", "T_C_01", "T_Version_00", "T_Version_00_TW", "T_AoCVersion_00", "T_AoCVersion_00_TW",
+                                "Logo_W_00", "Logo_W_01", "Logo_W_00_JPja_00", "Logo_W_00_JPja", "P_Blur_08", "P_BigTextSh_00", "T_DummyText_01", "T_Trial_00", "T_DummyText_00", "FBLayout_00", "FBLayout_02", "FBLayout_01", "FBLayout_03"],
                     'PaSaveIcon_00': ['N_Capture_00', "N_InOut_00", "N_Glow_00"],
+                    'Rupee_00': ['W_Base_00', "T_Rupee_00", "P_Icon_00", "Pa_PlusMinus_00"],
                 }
 
     def patch_ui_layouts(direction):
@@ -62,31 +67,43 @@ def patch_blarc(aspect_ratio, HUD_pos, romfs_folder):
                 command = "Ignoring"
         elif operation in ["shift_x", "shift_y"]:
             command = "Shifting"
-        
+
         print(f"{command} {pane} of {filename}")
-        
+
         offset_dict = {'shift_x': 0x40, 'shift_y': 0x48, 'scale_x': 0x70, 'scale_y': 0x78}
         modified_name = filename + "_name"
-        
+
         # Get all paths for the given filename
         paths = file_paths.get(modified_name, [])
         if not paths:
             # If no paths are found, create a default path and add it to the list
             default_path = os.path.join(romfs_folder, "region_common", "ui", "GameMain", "blyt", f"{filename}.bflyt")
             paths.append(default_path)
-        
+
         for full_path_of_file in paths:
             with open(full_path_of_file, 'rb') as f:
                 content = f.read().hex()
-            
+
             start_rootpane = content.index(b'RootPane'.hex())
+            
+            # Try to find the specified pane
             pane_hex = str(pane).encode('utf-8').hex()
-            start_pane = content.index(pane_hex, start_rootpane)
+            try:
+                start_pane = content.index(pane_hex, start_rootpane)
+            except ValueError:
+                # If "N_In_Out_00" is passed and not found, fall back to "RootPane"
+                if pane == "N_InOut_00":
+                    print(f"Pane {pane} not found. Using RootPane instead.")
+                    start_pane = start_rootpane
+                else:
+                    raise ValueError(f"Pane {pane} not found in {filename}.")
+
             idx = start_pane + offset_dict[operation]
             content_new = content[:idx] + float2hex(value) + content[idx+8:]
-            
+
             with open(full_path_of_file, 'wb') as f:
                 f.write(bytes.fromhex(content_new))
+
 
 
             
@@ -138,15 +155,24 @@ def patch_blarc(aspect_ratio, HUD_pos, romfs_folder):
             if name in do_not_scale_rootpane:
                     print(f"Skipping RootPane scaling of {name}")
             if name not in do_not_scale_rootpane:
-                patch_blyt(name, 'RootPane', 'scale_x', s1)
+                patch_blyt(name, 'N_InOut_00', 'scale_x', s1)
             if name in rootpane_by_y:
-                patch_blyt(name, 'RootPane', 'scale_y', 1/s1)
-                patch_blyt(name, 'RootPane', 'scale_x', 1)
+                patch_blyt(name, 'N_InOut_00', 'scale_y', 1/s1)
+                patch_blyt(name, 'N_InOut_00', 'scale_x', 1)
 
         patch_blyt('Title_00', 'Black8_00', 'scale_x', 1/s1)
 
-        # patch_blyt('Title_00', 'N_InOut_00', 'scale_x', 1/s1)
+        # patch_blyt('Title_00', 'N_InOut_00', 'scale_x', s1)
+        patch_blyt('Title_00', 'P_BackGround_00', 'scale_x', 1/s1)
         # patch_blyt('Title_00', 'N_InOut_00', 'scale_y', 1/s1)
+
+        patch_blyt('Title_00', 'P_Blur_00', 'scale_x', 1/s1)
+        patch_blyt('Title_00', 'P_Blur_02', 'scale_x', 1/s1)
+        patch_blyt('Title_00', 'P_Blur_01', 'scale_x', 1/s1)
+        patch_blyt('Title_00', 'P_Blur_04', 'scale_x', 1/s1)
+        patch_blyt('Title_00', 'P_Blur_03', 'scale_x', 1/s1)
+        patch_blyt('Title_00', 'P_Blur_05', 'scale_x', 1/s1)
+        patch_blyt('Title_00', 'P_Blur_07', 'scale_x', 1/s1)
 
         if HUD_pos == 'corner':
             print("Shifitng elements for corner HUD")
